b1f9f26b718d40a69efcf06423084f391ecc7a8e
Soyeon Park <aqua343@gmail.com>
2023-05-31 16:09:22 +0900

    hell world 추가!!!!

--- src/main/java/com/happybaechuchu/Application.java
+++ src/main/java/com/happybaechuchu/Application.java
@@ -3,6 +3,7 @@ package com.happybaechuchu;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello world!!!");
+        System.out.println("hell world!!!");
 
     }
 }
